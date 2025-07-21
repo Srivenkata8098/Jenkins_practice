8dcb54b9fc526ee6397622ac80cdd85b883d9db3
Srivenkata8098 <narayana8098@gmail.com>
2025-07-21 05:34:52 +0000

    Adding first CI project

---
+++ CI-1/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java App!");
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
+
