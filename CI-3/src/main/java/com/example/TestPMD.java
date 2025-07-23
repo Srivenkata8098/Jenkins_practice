b4ab6779a2f1a8ef2318250d2112a9faca7cfe63
Srivenkata8098 <narayana8098@gmail.com>
2025-07-23 13:07:56 +0530

    Update TestPMD.java

--- CI-3/src/main/java/com/example/TestPMD.java
+++ CI-3/src/main/java/com/example/TestPMD.java
@@ -1,9 +1,1 @@
-package com.example;
-
-public class TestPMD {
-    public void methodWithIssue() {
-        int unused = 10; // PMD: UnusedLocalVariable
-        System.out.println("This method has a PMD violation.");
-    }
-}
 
