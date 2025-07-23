274e721b12a4321d97895b7eddf2b4e1fcf934df
Sri <srivenkata.atti@gmail.com>
2025-07-23 13:06:32 +0530

    removed test

--- CI-3/src/main/java/com/example/TestPMD.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class TestPMD {
-    public void methodWithIssue() {
-        int unused = 10; // PMD: UnusedLocalVariable
-        System.out.println("This method has a PMD violation.");
-    }
-}
-
