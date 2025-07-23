8bed614f3edbce58f17dd8d71871e2dd2964b689
Sri <srivenkata.atti@gmail.com>
2025-07-23 13:01:09 +0530

    new tets file

--- CI-3/src/main/java/com/example/App.java
+++ CI-3/src/main/java/com/example/App.java
@@ -1,19 +1,9 @@
-/*
+
 package com.example;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from the CI-3 Java Project!");
-    }
-}
-*/
-
-package com.example;
-
-public class TestPMD {
-    public void methodWithIssue() {
-        int unused = 10; // PMD: UnusedLocalVariable
-        System.out.println("This method has a PMD violation.");
     }
 }
 
