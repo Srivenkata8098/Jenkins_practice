bf95b67f1942f3f88ebdeebf2a63b55ec7fcc70a
Sri <srivenkata.atti@gmail.com>
2025-07-28 10:37:29 +0530

    commiting new CI-8

---
+++ CI-8/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoApplicationTests {
+    @Test
+    public void testHelloWorld() {
+        assertEquals(1, 1); // Dummy test
+    }
+}
