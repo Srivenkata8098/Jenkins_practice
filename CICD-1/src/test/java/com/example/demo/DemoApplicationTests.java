64ff89767eb3d662e448b83e5a7f4dae7ae64e4d
Sri <srivenkata.atti@gmail.com>
2025-08-03 13:38:11 +0530

    commit

--- CICD-1/src/test/java/com/example/demo/DemoApplicationTests.java
+++ CICD-1/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class DemoApplicationTests {
+
     @Test
     void contextLoads() {
-        assertTrue(true);
     }
 }
